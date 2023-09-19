6807513b6584e31643e438aaffc1a4e2dec60f77
Suraj Sharma <suraj-sharma@live.com>
2023-09-19 21:11:53 +0200

    Initial commit

---
+++ src/book.py
@@ -0,0 +1,6 @@
+class Book:
+    title = ""
+    pages = 0
+    pages_read = 0
+    author = ""
+    cost = 0
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import random
+import tkinter as tk
+
+import book
+book1 = book.Book()
+
+root = tk.Tk()
+root.title("TextBox Input")
+root.geometry('400x200')
+
+root.mainloop()
