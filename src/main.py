356322e1707dcf6d3a916b0728cdd24e0c75359b
Suraj Sharma <suraj-sharma@live.com>
2023-09-22 21:14:31 +0200

    Added menu bar

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
 import random
 import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 import book
-book1 = book.Book()
 
-root = tk.Tk()
-root.title("TextBox Input")
-root.geometry('400x200')
+gBooks = [];
 
+gBooks.append(book.Book())
+
+#Window Setup
+root = ttk.Window(themename='journal')
+root.title("BookWormPy")
+root.geometry('1280x920')
+
+#Top menu bar
+menuBar = ttk.Menu(master = root)
+filemenu = ttk.Menu(menuBar, tearoff=0)
+filemenu.add_command(label="New")
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menuBar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = ttk.Menu(menuBar, tearoff=0)
+helpmenu.add_command(label="Help Index")
+helpmenu.add_command(label="About...")
+menuBar.add_cascade(label="Help", menu=helpmenu)
+root.config(menu=menuBar)
+
+#Side menu
+
+#Run app
 root.mainloop()
